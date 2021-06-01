7d2ed692ea3d8ad623162d3c816bbc202d9b8cd7
Damien Vesper <ldamienvesper@gmail.com>
2021-06-01 18:34:47 -0500

    no style

--- src/routes/errors/404.tsx
+++ src/routes/errors/404.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div className="pt-12">
-            <h1 className="text-9xl font-medium mt-9 mb-4 text-white text-center" style={{}}>404</h1>
+            <h1 className="text-9xl font-medium mt-9 mb-4 text-white text-center">404</h1>
             <h3 className="text-white text-center">Not Found</h3>
         </div>
     );
