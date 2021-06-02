f71ceb33fc272bd47d37d72953f02c4af486d67d
Damien Vesper <ldamienvesper@gmail.com>
2021-06-01 22:47:22 -0500

    legit does nothing

---
+++ src/routes/Servers.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Servers = () => {
+    return (
+        <div className="pt-12">
+            <h1 className="text-4xl text-center">Our Servers</h1>
+        </div>
+    );
+};
+
+export default Servers;
