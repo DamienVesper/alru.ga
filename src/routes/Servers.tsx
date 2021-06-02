b1655b664c03190db89000310a8464bd59b473b5
Damien Vesper <ldamienvesper@gmail.com>
2021-06-01 22:54:17 -0500

    finish up servers for now

--- src/routes/Servers.tsx
+++ src/routes/Servers.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Servers = () => {
     return (
-        <div className="pt-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Our Servers</h1>
+        <div className="pt-12 container mx-5 mb-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-10">
+                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Servers</h1>
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-400">Here are the servers that we offer on our network.</p>
+            </div>
         </div>
     );
 };
