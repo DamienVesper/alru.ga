44e9d54cb146389cd3d2dd26d0bfdc12622526d9
Damien Vesper <ldamienvesper@gmail.com>
2021-06-01 22:48:08 -0500

    fix up the text

--- src/routes/Servers.tsx
+++ src/routes/Servers.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Servers = () => {
     return (
         <div className="pt-12">
-            <h1 className="text-4xl text-center">Our Servers</h1>
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Our Servers</h1>
         </div>
     );
 };
